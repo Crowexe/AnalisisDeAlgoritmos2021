f0d02e801981d9fe52c0ab082e154cb9e9f64424
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2021-11-23 17:51:07 -0600

    Codeforce problems added

---
+++ codeforces/pizzaforce.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+    long long int resolver(long long int n) {
+        if(n % 6 == 0) return n/6 * 15;
+        if(n % 8 == 0) return n/8 * 20;
+        if(n % 10 == 0) return n/10 * 25;
+        long long int a;
+		a = (n/2)*5;
+
+			while(a%(5/2) != 0) {
+				a = a + 1;
+			}
+
+        return a;
+
+    }
+
+
+    int main() {
+        long long int t = 0;
+        long long int n = 0;
+        long long int i = 0;
+        scanf("%d", &t);
+        for(i = 0; i < t; i++) {
+            scanf("%lld", &n);
+            printf("%lld\n", resolver(n));
+        }
+        return 0;
+    }
